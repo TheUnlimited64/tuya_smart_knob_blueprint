c6ee86f6e72b386b6e0d4ad5323d3d550859d630
theunlimited64 <theunlimited64@gmail.com>
2024-11-24 12:56:08 +0100

    readme + cleanup of device_config.ts

--- device_config.ts
+++ device_config.ts
@@ -1,4 +1,4 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee')
 const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
@@ -6,14 +6,13 @@ const e = exposes.presets;
 const ea = exposes.access;
 
 const definition = {
-    // Copy your modified version here
     fingerprint: [
-        {modelID: 'TS004F', manufacturerName: '_TZ3000_4fjiwweb'},
-        {modelID: 'TS004F', manufacturerName: '_TZ3000_uri7ongn'},
-        {modelID: 'TS004F', manufacturerName: '_TZ3000_ixla93vd'},
-        {modelID: 'TS004F', manufacturerName: '_TZ3000_qja6nq5z'},
-        {modelID: 'TS004F', manufacturerName: '_TZ3000_abrsvsou'},
-        {modelID: 'TS004F', manufacturerName: '_TZ3000_402vrq2i'},
+        { modelID: 'TS004F', manufacturerName: '_TZ3000_4fjiwweb' },
+        { modelID: 'TS004F', manufacturerName: '_TZ3000_uri7ongn' },
+        { modelID: 'TS004F', manufacturerName: '_TZ3000_ixla93vd' },
+        { modelID: 'TS004F', manufacturerName: '_TZ3000_qja6nq5z' },
+        { modelID: 'TS004F', manufacturerName: '_TZ3000_abrsvsou' },
+        { modelID: 'TS004F', manufacturerName: '_TZ3000_402vrq2i' },
     ],
     model: 'ERS-10TZBVK-AA',
     vendor: 'Tuya',
@@ -29,7 +28,6 @@ const definition = {
         fz.battery,
     ],
     toZigbee: [tz.tuya_operation_mode],
-
     exposes: [
         e.action([
             'toggle',
@@ -47,17 +45,18 @@ const definition = {
             'rotate_right',
         ]),
         e.numeric("action_brightness_delta", ea.STATE).withValueMin(-255).withValueMax(255),
-        e.numeric('action_step_size', ea.STATE).withValueMin(0).withValueMax(255),
+        e.numeric('action_step_size', ea.STATE).withValueMin(-255).withValueMax(255),
         e.numeric('action_transition_time', ea.STATE).withUnit('s'),
         e.numeric('action_rate', ea.STATE).withValueMin(0).withValueMax(255),
         e.battery(),
         e
             .enum('operation_mode', ea.ALL, ['command', 'event'])
-            .withDescription('Operation mode: "command" - for group control, "event" - for clicks'),    ],
+            .withDescription('Operation mode: "command" - for group control, "event" - for clicks'),
+    ],
     configure: async (device, coordinatorEndpoint) => {
         const endpoint = device.getEndpoint(1);
         await endpoint.read('genBasic', [0x0004, 0x000, 0x0001, 0x0005, 0x0007, 0xfffe]);
-        await endpoint.write('genOnOff', {tuyaOperationMode: 1});
+        await endpoint.write('genOnOff', { tuyaOperationMode: 1 });
         await endpoint.read('genOnOff', ['tuyaOperationMode']);
         try {
             await endpoint.read(0xe001, [0xd011]);
@@ -67,7 +66,8 @@ const definition = {
         await endpoint.read('genPowerCfg', ['batteryVoltage', 'batteryPercentageRemaining']);
         await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
         await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
-        await reporting.batteryPercentageRemaining(endpoint);    },
+        await reporting.batteryPercentageRemaining(endpoint);
+    },
 };
 
 module.exports = definition;
---
+++ readme.md
@@ -0,0 +1,55 @@
+# Tuya Smart Knob blueprint for dimming with Zigbee2Mqtt
+
+I'm happy to annouce that it's now possible to use the Tuya Smart Knob for perfect dimming with zigbee2mqtt in home assistant!
+
+## Installation/Usage
+
+### Zigbee2Mqtt
+
+As of now (24.11.2024) the required change is not in zigbee2mqtt converter yet, so it requires to use an external converter. For that you'll need access to the filesystem. For examle file-editor, vscode server addon, ssh etc.
+
+1. create a folder named custom_devices next to the configuration.yaml of zigbee2mqtt. In homeassistant it's located in `/homeassistant/zigbee2mqtt/`
+2. create a file named `tuya_new.js` or how you like to name your converter and copy the contents of device_config.ts to your created file.
+3. go to the zigbee2mqtt UI and click on settings
+4. navigate to the external converters tab
+5. add entry for your newly created file. eg: `custom_devices/tuya_new.js`
+6. click submit and restart zigbee2mqtt addon.
+
+You should now see in your device page under details a new entry called `Action brightness delta`. Make sure the smart knob is in command mode. To change the mode quickly click the smart knob 3 times, it will change the mode.
+
+## Blueprint
+
+The blueprint is the second part that allows dimming or even volume control.
+
+[![Open your Home Assistant instance and show the blueprint import dialog with a specific blueprint pre-filled.](https://my.home-assistant.io/badges/blueprint_import.svg)](https://my.home-assistant.io/redirect/blueprint_import/?blueprint_url=https%3A%2F%2Fgithub.com%2FTheUnlimited64%2Ftuya_smart_knob_blueprint%2Fblob%2Fmaster%2Fblueprint.yaml)
+
+### Usage example
+
+The blueprint exposes the `delta_value` variable to be used for smart dimming. It contains how many ticks where send by the action of the smart knob. You can also control your volume control or build even more complex automations.
+
+
+To use the variables you need to use the yaml editor, instead of the visual editor of the automation. For that click on the three dots and select yaml editor.
+
+
+```
+description: ""
+alias: Universal Smart Knob Control
+use_blueprint:
+  path: TheUnlimited64/blueprint.yaml
+  input:
+    brightness_sensor: sensor.nachttisch_smart_knob_action_brightness_delta
+    click_sensor: sensor.nachttisch_smart_knob_action
+    rotation_action:
+      - target:
+          entity_id: light.schlafzimmer_haupt_lampe
+        data:
+          brightness_step_pct: "{{ delta_value * 10 }}"
+          transition: 0.5
+        action: light.turn_on
+    click_action:
+      - target:
+          entity_id: light.schlafzimmer_haupt_lampe
+        action: light.toggle
+        data:
+          transition: 0.5
+```
